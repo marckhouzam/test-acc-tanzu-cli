b81b7771d0e7a69107b733bb69e34cb5c81ece38
Thomas Risberg <trisberg@pivotal.io>
2022-03-08 08:47:22 -0500

    Set version to v1.1.1-dev

--- cmd/plugin/accelerator/main.go
+++ cmd/plugin/accelerator/main.go
@@ -34,7 +34,7 @@ func main() {
 	ctx := context.Background()
 	p, err := plugin.NewPlugin(&tanzucliv1alpha1.PluginDescriptor{
 		Name:           "accelerator",
-		Version:        "v1.2.0-dev",
+		Version:        "v1.1.1-dev",
 		Description:    "Manage accelerators in a Kubernetes cluster",
 		Group:          tanzucliv1alpha1.BuildCmdGroup,
 		CompletionType: tanzucliv1alpha1.NativePluginCompletion,
