aff358468694bf600f79e983d58c29503c560fe0
Thomas Risberg <trisberg@pivotal.io>
2022-05-25 11:01:52 -0400

    Pre-release v1.2.0-build.1

--- cmd/plugin/accelerator/main.go
+++ cmd/plugin/accelerator/main.go
@@ -34,7 +34,7 @@ func main() {
 	ctx := context.Background()
 	p, err := plugin.NewPlugin(&tanzucliv1alpha1.PluginDescriptor{
 		Name:           "accelerator",
-		Version:        "v1.2.0-dev",
+		Version:        "v1.2.0-build.1",
 		Description:    "Manage accelerators in a Kubernetes cluster",
 		Group:          tanzucliv1alpha1.BuildCmdGroup,
 		CompletionType: tanzucliv1alpha1.NativePluginCompletion,
