4e9a2ea5c7b89e792c27d993d6e2d10e99644098
Diego Wong <japo2300@gmail.com>
2022-11-21 13:46:32 -0500

    Set version to v1.4.0-dev

--- cmd/plugin/accelerator/main.go
+++ cmd/plugin/accelerator/main.go
@@ -34,7 +34,7 @@ func main() {
 	ctx := context.Background()
 	p, err := plugin.NewPlugin(&tanzucliv1alpha1.PluginDescriptor{
 		Name:           "accelerator",
-		Version:        "v1.4.0-build.1",
+		Version:        "v1.4.0-dev",
 		Description:    "Manage accelerators in a Kubernetes cluster",
 		Group:          tanzucliv1alpha1.BuildCmdGroup,
 		CompletionType: tanzucliv1alpha1.NativePluginCompletion,
